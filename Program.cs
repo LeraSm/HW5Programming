5288edb6b82ddf7058889ee8bffe865ebf574e92
Lera <badasyan.lera@mail.ru>
2022-08-23 22:39:10 +0300

    added decision for task 38

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// Console.WriteLine("Hello, World!");
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// double[] RandomArray = new double [10];
+//   for(int i = 0; i< 10; i++)
+//   {
+//   RandomArray[i]= new Random().Next(15,50);
+//   Console.Write(RandomArray[i] + "|");
+//   }
+    
+// double max = RandomArray[0];
+// double min = RandomArray[0];
+
+// for (int i = 1; i < RandomArray.Length; i++)
+//   {
+//     if (max < RandomArray[i])
+//     {
+//       max = RandomArray[i];
+//     }
+//         if (min > RandomArray[i])
+//     {
+//       min = RandomArray[i];
+//     }
+//   }
+// double difference = max - min;
+ 
+// Console.WriteLine($"The difference from {max} and {min} = {difference}");
